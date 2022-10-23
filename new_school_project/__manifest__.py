6c693314b471bc03d06d0f55f7a55f92f88d32e5
Thineisan20 <thineisan2017@gmail.com>
2022-10-23 23:38:15 +0630

    School Project

---
+++ new_school_project/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':"New School Project",
+    'license':'LGPL-3',
+    'sequence' : '-100',
+    'author':'TES',
+    'depends':[
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/school_info_view.xml',
+        'views/section_view.xml',
+        'views/attendance_view.xml',
+        'views/roll_call_view.xml'
+    ]
+}
---
+++ new_school_project/models/__init__.py
@@ -0,0 +1,4 @@
+from . import school_project
+from . import section
+from . import school_attendance
+from . import roll_call
